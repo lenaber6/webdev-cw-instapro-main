653250b40b7bdfcb4c3cd6a74c51e084afe668d4
lenaber6 <lenaber6@mail.ru>
2024-03-29 12:35:36 +0300

    scripts added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-export const entry = " ./index.js";
-export const output = {
-    filename: "main.js" // Выходной файл, который подключаем к HTML
-    // Обратите внимание, сохранится он по пути "./dist/main.js"
-};
+module.exports = {
+    entry: "./index.js", // Входной файл, в котором мы пишем свой код
+    output: {
+        filename: "main.js" // Выходной файл, который подключаем к HTML
+					// Обратите внимание, сохранится он по пути "./dist/main.js"
+    }
+}
